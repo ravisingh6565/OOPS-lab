3f8f51c518fda190514243f619f740adeb8fab8a
ravisuneel6565 <ravisuneel6262@gmail.com>
2024-04-29 14:46:59 +0530

    .

--- loop.java
+++ loop.java
@@ -1,9 +1,14 @@
 class Display{
     void display(){
+        int data[] = {1,2,3,4,5,6,7};
         System.out.println("hello Ravi Singh");
+        
     }
 
 }
-public static void main(String args[]){
- Display obj = new Display();
+class Loop{
+    public static void main(String args[]){
+    Display obj = new Display();
+    obj.display();
+    }
 }
