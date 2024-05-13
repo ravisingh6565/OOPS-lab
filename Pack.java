43414c0a0c1e85cf0fc1cbc6a4e78add23834186
ravisuneel6565 <ravisuneel6262@gmail.com>
2024-05-13 14:39:48 +0530

    .

--- Pack.java
+++ Pack.java
@@ -1,5 +1,8 @@
+import sum.Sum;
 class Pack{
     public static void main(String args[]){
-        
+
+        Sum s1= new Sum();
+        s1.add(23,23);
     }
 }
---
+++ sum/Sum.java
@@ -0,0 +1,10 @@
+package sum;
+
+public class Sum {
+    // public Sum() {
+  
+    // }
+    public void add(int a, int b) {
+        System.out.println("Total Sum is : " + (a + b));
+    }
+}
